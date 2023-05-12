a45945a3867b5b6f1bf3a475477ccc3d384a047a
star1 <star1@shengshaoxingdeMacBook-Pro.local>
2023-05-12 22:51:17 +0800

    feat: nft prompt

--- src/pages/dataMarket/index.tsx
+++ src/pages/dataMarket/index.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@chakra-ui/react'
-import { Empty, Menu, MenuProps, Popconfirm, Space, Table } from 'antd';
+import { Empty, Menu, MenuProps, Popconfirm, Space, Table, Tooltip } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import Head from 'next/head'
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import nft1 from '../../../public/images/nft1.svg'
+import nft2 from '../../../public/images/nft2.svg'
+import Bitmap from '../../../public/images/Bitmap.svg'
+import Image from 'next/image'
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -111,6 +115,58 @@ const columns: ColumnsType<DataType> = [
     key: 'action',
   },
 ];
+
+interface nftDataType {
+  key: string,
+  img: any,
+  title: string,
+  prompt: string,
+  price: string,
+}
+
+const nftColumns: ColumnsType<nftDataType> = [
+  {
+    title: 'NFT',
+    dataIndex: 'title',
+    key: 'title',
+    render: (text, record) => <div className='bg-[#333] inline-block rounded-lg shadow-lg'>
+      <Image src={record.img} alt='' />
+      <p className='p-2 flex justify-between items-center'>
+        <span>{text}</span>
+        <div style={{ filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))", padding: 4, boxShadow: '-1px -1px 5px #383838' , borderRadius: '50%', }}>
+         <Image width={20} src={Bitmap} alt=''></Image>
+        </div>
+      </p>
+    </div>
+  },
+  {
+    title: 'prompt',
+    dataIndex: 'prompt',
+    key: 'prompt',
+    width: "30%",
+    render: (prompt: string) => (
+      <Tooltip title={prompt} placement="right" overlayClassName="w-[600px]" autoAdjustOverflow={false}>
+        {prompt.substring(0, 160) + '...'}
+      </Tooltip>
+    ),
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+    render: (text) => <><span className='text-2xl font-bold'>{text}</span><span className='text-base ml-2'>BNB</span></>
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: () => <a className='text-[#BBE7E6] text-3xl font-bold'>Buy</a>
+  },
+];
+
+const dataNfts: nftDataType[] = [
+  { key: uuidv4(), title: 'No.2518', price: '1.12443554', img: nft1, prompt: `The portrait is centered,(close-up:1.0),(nagative space:0.8),(solo)(portrait:1.1), (head and shoulder: full image 1:1), Azuki, head and shoulder_only, profile, masterpiece, best quality, boxer, 1girl, solo, facing left, down, open_mouth, smiling, happy emotion, white shirt, hair_ornament, green hair, Pink_cloat, light green hair, :d, multicolored_hair, blue eyes, wings, white_hairclip, blue eyes, hair_bun, chibi, ponytail, black_choker, blush_stickers, hands behind body, both hands in pocket, animal ears, cherry blossoms, Dehya, elderly woman, (floating colorful {wind|water magic|ink|crystals|ice|fire|flame|lightning|web|rocks|sand|particles|sparkles|blood}:1.0),(many colorful {soap bubbles|clouds|thorns|spikes|vines|currents|spirals|halos|wings|tatoos|cubes|octahedron}:1.0),, feathered_wings, angel_wings, white_wings, <lora:mixProV3_v3-000002:0.8>`},
+  { key: uuidv4(), title: 'No.10', price: '2.2121212', img: nft2, prompt: `The portrait is centered,(close-up:1.0),(nagative space:0.8),(solo)(portrait:1.1), (head and shoulder: full image 1:1), Azuki, head and shoulder_only, profile, masterpiece, best quality, boxer, 1girl, solo, facing left, down, open_mouth, smiling, happy emotion, white shirt, hair_ornament, green hair, Pink_cloat, light green hair, :d, multicolored_hair, blue eyes, wings, white_hairclip, blue eyes, hair_bun, chibi, ponytail, black_choker, blush_stickers, hands behind body, both hands in pocket, animal ears, cherry blossoms, Dehya, elderly woman, (floating colorful {wind|water magic|ink|crystals|ice|fire|flame|lightning|web|rocks|sand|particles|sparkles|blood}:1.0),(many colorful {soap bubbles|clouds|thorns|spikes|vines|currents|spirals|halos|wings|tatoos|cubes|octahedron}:1.0),, feathered_wings, angel_wings, white_wings, <lora:mixProV3_v3-000002:0.8>`}
+]
 
 const data: DataType[] = [
   { key: uuidv4(), name: 'Bucket', description: 'Bucket des', type: 'bucket',
@@ -223,6 +279,9 @@ export default function Datamarket() {
                   }}
                   dataSource={data}
                 /> : null}
+                { activeMenu === "nftPrompt"
+                  ? <Table dataSource={dataNfts} columns={nftColumns} pagination={false} /> 
+                  : null }
               </div>
             </div>
           </div>
